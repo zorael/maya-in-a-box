5209d8a11e3aeff17967d3dc5b8c9f6a9847d817
JR <zorael@gmail.com>
2026-09-10 18:54:06 +0200

    Add setup-exports.sh

--- Containerfile
+++ Containerfile
@@ -107,6 +107,8 @@ COPY --chmod=0755 first-run.sh /opt/maya-install/
 
 COPY --chmod=0755 setup-licensing.sh /opt/maya-install/
 
+COPY --chmod=0755 setup-exports.sh /opt/maya-install/
+
 COPY --chmod=0644 Autodesk-Maya2027.desktop /usr/local/share/applications/
 
 COPY --chmod=0644 com.autodesk.AdskIdentityManager.desktop /opt/maya-install/
---
+++ setup-exports.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOST_HOME="/run/host/home/$(id -un)"
+
+if [[ ! -d "$HOST_HOME" ]]; then
+    echo "[x] Failed to resolve host home path" >&2
+    exit 1
+fi
+
+MAYA_RUN_PATH="$HOST_HOME/.local/bin/maya-run"
+
+if [[ -f "$MAYA_RUN_PATH" ]]; then
+    echo "[!] wrapper script already exists; not re-exporting" >&2
+else
+    echo "[*] exporting wrapper script"
+    mkdir -p "$HOST_HOME/.local/bin"
+    distrobox-export --bin /usr/local/bin/maya-run
+fi
+
+# We don't know CONTAINER_NAME from build.sh but we do know CONTAINER_ID
+DESKTOP_FILE_NAME="Autodesk-Maya2027.desktop"
+DESKTOP_FULL_NAME="$CONTAINER_ID-$DESKTOP_FILE_NAME"
+
+if [[ -f "$HOST_HOME/.local/share/applications/$DESKTOP_FULL_NAME" ]]; then
+    echo "[!] '$DESKTOP_FULL_NAME' already exists; not re-exporting" >&2
+else
+    echo "[*] exporting .desktop file"
+    echo
+    distrobox-export --app "$DESKTOP_FILE_NAME"
+    echo
+fi
